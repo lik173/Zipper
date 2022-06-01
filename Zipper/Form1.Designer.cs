65d544eac48bdc718446da0c33c41b2fbadd3cb7
lik173 <70771696+lik173@users.noreply.github.com>
2022-06-01 22:33:49 +0300

    Add files via upload

---
+++ Zipper/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Zipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deZipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.deZipToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(120, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(36, 20);
+            this.fileToolStripMenuItem.Text = "Zip";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // deZipToolStripMenuItem
+            // 
+            this.deZipToolStripMenuItem.Name = "deZipToolStripMenuItem";
+            this.deZipToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.deZipToolStripMenuItem.Text = "De Zip";
+            this.deZipToolStripMenuItem.Click += new System.EventHandler(this.deZipToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 28);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Zipper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deZipToolStripMenuItem;
+    }
+}
+
---
+++ Zipper/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zipper
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        internal void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        internal void zipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        openFileDialog1.InitialDirectory = "%USERPROFILE%";
+        //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+        //openFileDialog1.FilterIndex = 2;
+        openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                string IFile = openFileDialog1.FileName;
+
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "zip files (*.dfl)|*.dfl|All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string OFile = saveFileDialog1.FileName;
+                    using FileStream originalFileStream = File.Open(IFile, FileMode.Open);
+                    using FileStream compressedFileStream = File.Create(OFile);
+                    using var compressor = new DeflateStream(compressedFileStream, CompressionMode.Compress);
+                    originalFileStream.CopyTo(compressor);
+                }
+            }
+        }
+
+        private void deZipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.InitialDirectory = "%USERPROFILE%";
+            //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            //openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+
+                //Get the path of specified file
+                string IFile = openFileDialog1.FileName;
+
+
+
+
+
+
+
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string OFile = saveFileDialog1.FileName;
+                    using FileStream compressedFileStream = File.Open(IFile, FileMode.Open);
+                    using FileStream outputFileStream = File.Create(OFile);
+                    using var decompressor = new DeflateStream(compressedFileStream, CompressionMode.Decompress);
+                    decompressor.CopyTo(outputFileStream);
+                }
+            }
+        }
+    }
+}
